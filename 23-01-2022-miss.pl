2e22452b7166bc290c8b110a049202b04a523651
Ivo Stratev <ivo.stratev.tues@gmail.com>
2022-01-23 21:57:47 +0200

    Update 23-01-2022-miss.pl

--- 23-01-2022-miss.pl
+++ 23-01-2022-miss.pl
@@ -10,3 +10,7 @@ elem(N, A) :- N >= 2, N1 is N-1, N2 is N-2, elem(N1, A1), elem(N2, A2),
     A is 3 * A1 * A1 * A1 + 2 * A2 * A2.
 
 miss(X) :- range(0, X, N), not(elem(N, X)).
+
+% range(A, B, C) - C is int between A and B.
+range(A, B, A) :- A =< B.% =<, >=
+range(A, B, C) :- B > A, A1 is A + 1, range(A1, B, C).
